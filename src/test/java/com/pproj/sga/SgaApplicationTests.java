3a78d6e408aebe39f9acf0a052acead1da9b4635
Mardel Juma <mardel.mjj@gmail.com>
2023-09-08 09:00:42 +0200

    Repo init

---
+++ src/test/java/com/pproj/sga/SgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pproj.sga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
