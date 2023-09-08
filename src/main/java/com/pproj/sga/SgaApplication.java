3c329e67087d3bd61678166ae2ae4d9c938bb8e0
Mardel Juma <mardel.mjj@gmail.com>
2023-09-08 09:11:40 +0200

    Tests

--- src/main/java/com/pproj/sga/SgaApplication.java
+++ src/main/java/com/pproj/sga/SgaApplication.java
@@ -8,6 +8,12 @@ public class SgaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SgaApplication.class, args);
+		helloSpring("Hello World");
+		System.out.println("Hello Spring");
 	}
 
+	public static String helloSpring (String msg) {
+		System.out.println("Hello World" + msg);
+		return msg;
+	}
 }
